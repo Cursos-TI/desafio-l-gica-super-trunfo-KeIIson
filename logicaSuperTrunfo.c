121d707867f285f9a5fc4a6d09b5d516087e636c
Kellson <kelsonnb@gmail.com>
2025-09-22 22:30:04 +0000

    Atributos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -7,4 +7,30 @@ typedef struct {
     int velocidade;
     int inteligencia;
     int densidade_demografica;
-} 
+} Carta;
+
+void exibirOpcoes(int atributoEscolhido) {
+    printf("Escolha um atributo (diferente do primeiro, se já escolhido):\n");
+    if (atributoEscolhido != 1) {
+        printf("1 - Força\n");
+    }
+    if (atributoEscolhido != 2) {
+        printf("2 - Velocidade\n");
+    }
+    if (atributoEscolhido != 3) {
+        printf("3 - Inteligência\n");
+    }
+    if (atributoEscolhido != 4) {
+        printf("4 - Densidade Demográfica\n");
+    }
+}
+
+int obterValorAtributo(Carta c, int atributo) {
+    switch (atributo) {
+        case 1: return c.forca;
+        case 2: return c.velocidade;
+        case 3: return c.inteligencia;
+        case 4: return c.densidade_demografica;
+        default: return 0;
+    }
+}
